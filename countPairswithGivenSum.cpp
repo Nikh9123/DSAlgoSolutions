203524b8453344463415e80949ceee1271871f28
Nikhil Satyam <satyamnikhil1523@gmail.com>
2022-08-16 21:47:55 +0530

    solution

---
+++ countPairswithGivenSum.cpp
@@ -0,0 +1,50 @@
+// { Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function template for C++
+
+class Solution{   
+public:
+    int getPairsCount(int arr[], int n, int k) {
+        // code here
+        sort(arr, arr + n);
+    int x = 0, c = 0, y, z;
+    for (int i = 0; i < n - 1; i++) {
+        x = k - arr[i];
+       
+        // Lower bound from i+1
+        int y = lower_bound(arr + i + 1,
+                            arr + n, x) - arr;
+         
+        // Upper bound from i+1
+        int z = upper_bound(arr + i + 1,
+                            arr + n, x) - arr;
+        c = c + z - y;
+    }
+    return c;
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.getPairsCount(arr, n, k);
+        cout << ans << "\n";
+    }
+    
+    return 0;
+}  // } Driver Code Ends
