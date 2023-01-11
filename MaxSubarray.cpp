b8ce4a3affda8ab499df18320e8272838e89dd7f
Nikhil Satyam <satyamnikhil1523@gmail.com>
2023-01-11 12:54:54 +0530

    update push

--- MaxSubarray.cpp
+++ MaxSubarray.cpp
@@ -24,7 +24,20 @@ for(int i =0 ; i < n ;i++)
 }
 return res ;
 
-    //kadane's algorithm can be used easily
+    //kadane's algorithm can be used easily TC = O(n) 
+
+    /*Initialize:
+    max_so_far = INT_MIN
+    max_ending_here = 0
+
+Loop for each element of the array
+
+  (a) max_ending_here = max_ending_here + a[i]
+  (b) if(max_so_far < max_ending_here)
+            max_so_far = max_ending_here
+  (c) if(max_ending_here < 0)
+            max_ending_here = 0
+return max_so_far*/
 //     int sum = 0, maxSum = INT_MIN ;
 //   for(int i =0 ; i < n ; i++)
 //   {
