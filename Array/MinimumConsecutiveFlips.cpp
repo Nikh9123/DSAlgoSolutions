cb53314ecf15c9df457a5b8187074ac1629c084c
Nikhil Satyam <satyamnikhil1523@gmail.com>
2023-01-19 22:01:03 +0530

    update code

--- Array/MinimumConsecutiveFlips.cpp
+++ Array/MinimumConsecutiveFlips.cpp
@@ -1,37 +1,48 @@
-#include<bits/stdc++.h>
+// C++ program to find the minimum
+
+/*
+@https://www.geeksforgeeks.org/minimum-group-flips-to-make-binary-array-elements-same/
+
+*/
+// group flips in a binary array
+#include <iostream>
 using namespace std;
 
-void printGroups(int arr[], int n)
+void printGroups(bool arr[], int n)
 {
-    int count1 = 0, count0 = 0 ;
-   for(int i = 1 ; i < n ; i++)
-   {
-     if(arr[i] != arr[i-1])
-     {
-        if(arr[i] != arr[0])
-        {
-            cout<<"from "<<i << " to ";
-        }
-        else{
-            cout<<i-1<<endl;
-        }
-     }
-   }
 
-   //to handle the last element case if it is nor equal to first element [0,0,1,,1,0,0,1,1,0,1]
+    // Traverse through all array elements
+    // starting from the second element
+    for (int i = 1; i < n; i++) {
 
-   if(arr[n-1] != arr[0])
-   {
-    cout<<n-1<<endl;
-   }
+        // If current element is not same
+        // as previous
+        if (arr[i] != arr[i - 1]) {
+
+            // If it is same as first element
+            // then it is starting of the interval
+            // to be flipped.
+            if (arr[i] != arr[0])
+                cout << "From " << i << " to ";
+
+            // If it is not same as previous
+            // and same as first element, then
+            // previous element is end of interval
+            else
+                cout << (i - 1) << endl;
+        }
+    }
+
+    // Explicitly handling the end of
+    // last interval
+    if (arr[n - 1] != arr[0])
+        cout << (n - 1) << endl;
 }
 
-
-
-int main() {
-	
-     int arr[] = {0, 0, 1, 1, 0, 0, 1, 1, 0,1}, n = 10;
-
-     printGroups(arr, n);
-    
+int main()
+{
+    bool arr[] = { 0, 1, 1, 0, 0, 0, 1, 1 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    printGroups(arr, n);
+    return 0;
 }
