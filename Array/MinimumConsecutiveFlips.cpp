060aafaf1555070c56f8176c81e9c8da9134accb
Nikhil Satyam <satyamnikhil1523@gmail.com>
2023-01-26 22:12:29 +0530

    update dir

--- Array/MinimumConsecutiveFlips.cpp
+++ Array/MinimumConsecutiveFlips.cpp
@@ -2,7 +2,7 @@
 
 /*
 @https://www.geeksforgeeks.org/minimum-group-flips-to-make-binary-array-elements-same/
-
+0, 1, 1, 0, 0, 0, 1, 1
 */
 // group flips in a binary array
 #include <iostream>
@@ -11,52 +11,25 @@ using namespace std;
 void printGroups(bool arr[], int n)
 {
 
-    // Traverse through all array elements
-    // starting from the second element
-    // for (int i = 1; i < n; i++) {
-
-    //     // If current element is not same
-    //     // as previous
-    //     if (arr[i] != arr[i - 1]) {
-
-    //         // If it is same as first element
-    //         // then it is starting of the interval
-    //         // to be flipped.
-    //         if (arr[i] != arr[0])
-    //             cout << "From " << i << " to ";
-
-    //         // If it is not same as previous
-    //         // and same as first element, then
-    //         // previous element is end of interval
-    //         else
-    //             cout << (i - 1) << endl;
-    //     }
-    // }
-
-    // // Explicitly handling the end of
-    // // last interval
-    // if (arr[n - 1] != arr[0])
-    //     cout << (n - 1) << endl;
-
-
-    for (int i = 1; i < n; i++)
+for(int i = 1 ; i < n ; i++)
+{
+    if(arr[i] != arr[i-1])// comdition for checking that it is the begining of new grp to be flip
     {
-        if (arr[i] != arr[i - 1])
+        if(arr[i] != arr[0]) // condition for is array element that current element is not equal to first element
         {
-            if (arr[i] != arr[0])
-                cout << "from " << i << " to ";
-            else
-            {
-                cout << i - 1;
-                cout<<endl;
-            }
+            cout<<"from "<<i<<" to " ;
+        }
+        else{
+            cout<<i-1;
+            cout<<endl;
         }
     }
-    if(arr[n-1] != arr[0])
-    {
-        cout<<n-1;
-        cout<<endl;
-    }
+}
+if(arr[n-1] != arr[0] || n==1) //last terminating condition
+{
+    cout<<n-1;
+}
+
 }
 
 int main()
