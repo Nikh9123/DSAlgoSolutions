53b2fcb56e200f5344ffc57c4198b30ee2ccc475
Nikhil Satyam <satyamnikhil1523@gmail.com>
2023-02-22 02:17:30 +0530

    push code

---
+++ Array/Sorting/insertionSort.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+void iSort(int arr[],int n){
+    
+  for(int i = 1 ; i < n ; i++)
+  {
+      int key = arr[i];
+      for(int j= i-1 ; j >= 0 ; j--)
+      {
+          if(key < arr[j])
+          {
+              arr[j+1] = arr[j] ;
+              arr[j] = key;
+          }
+      }
+  }
+    
+}
+
+int main() {
+	
+    int arr[]={50,20,40,60,10,30};
+	
+	int n=sizeof(arr)/sizeof(arr[0]);
+	iSort(arr,n);
+	
+	for(auto x: arr)
+	    cout<<x<<" ";
+}
