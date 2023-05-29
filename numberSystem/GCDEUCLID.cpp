b2f3f3ab48fbfe8800453e825bff65db4a2bb2a8
Nikh9123 <satyamnkhil1524@gmail.com>
2023-05-29 10:40:18 +0530

    practice euclidian gcd

---
+++ numberSystem/GCDEUCLID.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int GCD(int a, int b)
+{
+  if (b == 0)
+  {
+    return a;
+  }
+  else
+  {
+    if (b > a)
+    {
+      return (b, a % b);
+    }
+    else
+    {
+      return (b, b % a);
+    }
+  }
+}
+int main()
+{
+  cout << GCD(3, 12);
+}
