a40af44260d381c03f1eee1862783e0f45b321e5
Nikh9123 <satyamnkhil1524@gmail.com>
2023-08-12 01:48:51 +0530

    adding new question in array section

---
+++ Array/maxConsecutiveOne.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+int maxConsecutiveOne(int arr[], int n)
+{
+  int count = 0, res = 0;
+  // for (int i = 0; i < n; i++)
+  // {
+  //   count = 0;
+  //   for (int j = i; j < n; j++)
+  //   {
+  //     if (arr[j] == 1)
+  //     {
+  //       count++;
+  //     }
+  //     else {
+  //       break;
+  //     }
+  //   }
+  //   res = max(res, count);
+  // }
+  // return res;
+
+  for (int i = 0; i < n; i++)
+  {
+    if (arr[i] == 1)
+    {
+      count++;
+      // cout << "val of i = " << i << " value of res = " << res << " val of count = " << count << endl;
+    res = max(res, count);
+    }
+    else {
+      count = 0;
+      // cout << "val of i = " << i << " value of res = " << res << " val of count = " << count << endl;
+    }
+  }
+  return res;
+}
+int main()
+{
+  int n = 12;
+  int arr[n] = { 1,1,1,0,0,1,1,0,1,1,1,1 };
+  cout << maxConsecutiveOne(arr, n);
+
+}
