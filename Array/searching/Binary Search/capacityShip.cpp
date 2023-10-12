8a9c380e26c4cf344df3c1685dd4de9d726a9588
nikh9123 <satyamnikhil1523@gmail.com>
2023-10-12 12:20:42 +0530

    starting binary search

---
+++ Array/searching/Binary Search/capacityShip.cpp
@@ -0,0 +1,21 @@
+//@https://leetcode.com/problems/capacity-to-ship-packages-within-d-days/description/
+#include<bits/stdc++.h>
+using namespace std ;
+
+int minAndmaxDaysReq(vector<int>nums)
+{
+  int n = nums.size();
+  int mini = INT_MAX ;
+  
+}
+int shipProducts(vector<int>nums)
+{
+
+}
+
+int main()
+{
+vector<int>arr = {1,2,3,4,5,6,7,8,9,10} ;
+int days = 5 ;
+int minDays = shipProducts(arr);
+}
